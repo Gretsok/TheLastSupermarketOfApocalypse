f5e073ca54f2d9eead2f7290d35a3cda4e92ec59
Pierre Vue <83540440+KeeMoh@users.noreply.github.com>
2023-03-05 12:24:36 +0100

    UI Score and links

--- TheLastSupermarketOfApocalypse/Assets/TLSOA/Kart/ScoreRegisterer.cs
+++ TheLastSupermarketOfApocalypse/Assets/TLSOA/Kart/ScoreRegisterer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace TLSOA.Kart
@@ -7,10 +8,11 @@ namespace TLSOA.Kart
         private int _totalScore = 0;
         public int TotalScore => _totalScore;
 
-
+        public Action<ScoreRegisterer> onScoreChange;
         public void AddScore(int scoreToAdd)
         {
             _totalScore += scoreToAdd;
+            onScoreChange?.Invoke(this);
         }
     }
 }
